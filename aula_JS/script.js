44e89f1291ba07989206c2c95c329588b11e18fd
Caio Moura <91078627+cmalheiros@users.noreply.github.com>
2022-05-03 21:42:12 -0300

    JS 2

--- aula_JS/script.js
+++ aula_JS/script.js
@@ -1,2 +1,12 @@
-alert("Hello, world")
-console.log("Ola, mundo")
+//Boolean, verdadeiro ou falso (true or false)
+//Null, vazio.
+//Underfined, indefinifo.
+//Number, número, sem ""
+/*String, caceia de caractere.*/
+
+var a = 2;
+var b ="palavra";
+var c = 58;
+console.log(a);
+console.log(b);
+console.log(a + c);
