19498afd3a0c6adcbfd52a03044d0a066e48ee21
Caio Moura <91078627+cmalheiros@users.noreply.github.com>
2022-05-03 21:59:16 -0300

    operadores matematicos

--- aula_JS/script.js
+++ aula_JS/script.js
@@ -1,8 +1,9 @@
-//Boolean, verdadeiro ou falso (true or false)
-//Null, vazio.
-//Underfined, indefinifo.
-//Number, número, sem ""
-/*String, caceia de caractere.*/
+/* variaveis
+Boolean, verdadeiro ou falso (true or false)
+Null, vazio.
+Underfined, indefinifo.
+Number, número, sem ""
+String, caceia de caractere.
 
 var a = 2;
 var b ="palavra";
@@ -10,3 +11,18 @@ var c = 58;
 console.log(a);
 console.log(b);
 console.log(a + c);
+*/
+
+/* operações aritimeticas
++ adição
+- subtração
+* multiplicação
+/ divisão
+% módulo que é o resultado do resto da divisão.
+++ incremento, adiciona mais 1
+-- decremento, diminui 1
+*/
+var d = 10;
+var e = 100;
+var f = e * d;
+console.log(f)
