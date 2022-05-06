fc2520292c3ed049c61fd42323c7285679aaf6b6
Caio Moura <91078627+cmalheiros@users.noreply.github.com>
2022-05-06 09:15:26 -0300

    atribuições js

--- aula_JS/script.js
+++ aula_JS/script.js
@@ -34,8 +34,50 @@ console.log(f)
 //-=  x -= y  x = x - y
 //*=  x *= y  x = x * y
 //%=  x %= y  x = x % y 
-*/
+
 
 var a = 10;
 a *= 3;
 console.log(a);
+*/
+
+/* operadores de comparação (tipo boleano)
+== igual a
+=== valor igual e igual
+! = não é igual
+!== não é igual ou não igual
+> maior que ou igual a 
+< menor de
+> = maior que ou igual a
+< = menor ou igual a
+
+var c = 55;
+
+console.log( c > 6);
+*/
+
+//operadores logicos
+// && e
+// || ou 
+// Negativo
+
+/*var idade = 15;
+
+var maior20 = idade >=20;
+var menor30 = idade <=30;
+var entre = maior20 && menor30;
+console.log("idade:", idade);
+console.log("Maior que 20:", maior20);
+console.log("Menor que 30:", menor30);
+console.log("Entre 20 e 30 anos:", entre);*/
+/*var idade = 8;
+
+var maior65 = idade >=65;
+var menor10 = idade <=10;
+var gratuidade = maior65 || menor10;
+
+console.log("idade:", idade);
+console.log("Maior que 65:", maior65);
+console.log("Menor que 10:", menor10);
+console.log("Tem direito a gratuidade?", gratuidade);
+*/
